da962ca8a66efc27df838a2f04bbf90c904c007e
Andrey Simurzin <asimurzin@gmail.com>
2011-04-14 13:48:38 +0400

    to simplify preparation wrong FOAM_VERSION

--- buoyantSimpleRadiationFlux/__init__.py
+++ buoyantSimpleRadiationFlux/__init__.py
@@ -24,30 +24,7 @@
 
 
 #--------------------------------------------------------------------------------------------
-argv = None
-import sys, os
-
-# Check is the pythonFlu installed
-try:
-   import Foam
-   pass
-except ImportError:
-   print 
-   print "You must install pythonFlu first( http://sourceforge.net/projects/pythonflu/files/)" 
-   print
-   os._exit( os.EX_UNAVAILABLE )
-   pass
-
-
-#--------------------------------------------------------------------------------------
 from Foam import FOAM_VERSION, FOAM_BRANCH_VERSION,FOAM_REF_VERSION
-if FOAM_VERSION( "<=", "010401" ):
-    from Foam.OpenFOAM import ext_Info
-    ext_Info() << "\n\n To use this solver, it is necessary to SWIG OpenFOAM-1.5 or higher\n"    
-    pass
-
-
-#----------------------------------------------------------------------------------------------
 if FOAM_VERSION( "==", "010500" ):
     from buoyantSimpleRadiationFlux.r1_5 import *
     pass
@@ -73,8 +50,14 @@ if FOAM_REF_VERSION( ">=", "010700" ):
 
 #--------------------------------------------------------------------------------------
 def entry_point():
-    import sys; argv = sys.argv
-    return main_standalone( len( argv ), argv )
+    try:
+       import sys; argv = sys.argv
+       return main_standalone( len( argv ), argv )
+    except NameError:
+       print
+       print "There is no implementation of the current OpenFOAM version"
+       print
+       pass
 
 
 #--------------------------------------------------------------------------------------
