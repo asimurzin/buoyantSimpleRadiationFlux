67662101a628512a94fab174435c67a4ffba490b
Andrey Simurzin <asimurzin@gmail.com>
2011-04-14 20:57:33 +0400

    to remove feature "-test"

--- buoyantSimpleRadiationFlux/r1_5/__init__.py
+++ buoyantSimpleRadiationFlux/r1_5/__init__.py
@@ -281,12 +281,6 @@ from Foam import FOAM_VERSION
 if FOAM_VERSION( "==", "010500" ):
     if __name__ == "__main__" :
         argv = sys.argv
-        if len(argv) > 1 and argv[ 1 ] == "-test":
-           argv = None
-           test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'local', 'r1.5', 'buoyantSimpleRadiationFoam', 'hotRadiationRoom' )
-           argv = [ __file__, "-case", test_dir ]
-           pass
-           
         os._exit( main_standalone( len( argv ), argv ) )
         pass
     pass
